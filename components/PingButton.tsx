b2037857d1ff5d230401195e90c33081a316207e
daaruu <sachansujal773@gmail.com>
2025-10-19 17:46:01 +0530

    Not Working Ping

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,11 +1,59 @@
 import { FC, useState } from 'react'
 import styles from '../styles/PingButton.module.css'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { PublicKey, sendAndConfirmTransaction, Transaction, TransactionInstruction } from '@solana/web3.js';
 
 export const PingButton: FC = () => {
 
-    const onClick = () => {
-        console.log('Ping!')
-    }
+	const PROGRAM_ID = 'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa'
+	const DATA_ACCOUNT_PUBKEY = 'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
+
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+
+    const onClick = async () => {
+		if( !connection ) {
+			console.error("Wallet not connected");
+			return;
+		}
+		if( !publicKey ) {
+			console.error("Connection unavaliable");
+			return;
+		}
+
+		try {
+			const programId = new PublicKey(PROGRAM_ID);
+			const programDataAccount = new PublicKey(DATA_ACCOUNT_PUBKEY);
+			const transaction = new Transaction();
+
+			const instruction = new TransactionInstruction({
+				keys: [
+					{
+						pubkey: programDataAccount,
+						isSigner:false,
+						isWritable: true,
+					},
+				],
+				programId,
+			});
+
+			transaction.add(instruction);
+
+			const signature = await sendTransaction(transaction, connection);
+			console.log(`Transaction Signature : ${signature}`);
+
+			const latestBlockhash = await connection.getLatestBlockhash();
+			await connection.confirmTransaction({
+				signature,
+				...latestBlockhash,
+			});
+			
+		}
+
+		catch (error) {
+			console.log(`Transaction Failed : ${error}`);
+		}
+    };
     
 	return (
 		<div className={styles.buttonContainer} onClick={onClick}>
---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode, useMemo } from "react";
+import {
+    ConnectionProvider,
+    WalletProvider
+} from "@solana/wallet-adapter-react"
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css")
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+
+  const endpoint = clusterApiUrl("devnet");
+  const wallets = useMemo(() => [new PhantomWalletAdapter], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} >
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
